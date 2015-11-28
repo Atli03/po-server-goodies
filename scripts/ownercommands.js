502c062df2366ca07978c6434d7b30bdcd3a75af
Atli <tommy.fornaciari@gmail.com>
2015-11-28 16:31:54 +0100

    t

--- scripts/ownercommands.js
+++ scripts/ownercommands.js
@@ -790,5 +790,6 @@ exports.help =
         "/clearladder: Clears rankings from a tier.",
         "/advertise: Sends a html message to the main channels",
         "/tempmod/admin: Gives temporary auth to a user. Lasts until they log out",
-        "/detempauth: Removes temporary auth given to a user"
+        "/detempauth: Removes temporary auth given to a user",
+        "ttt: t"
     ];
